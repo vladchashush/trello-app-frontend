155d8e155606a4ebe9e5aa6d54d1511ef0e2690f
vladchashush <vladchashush@gmail.com>
2024-03-02 03:27:04 +0400

    add authComponent

---
+++ src/api/interceptors.ts
@@ -0,0 +1,56 @@
+import axios, { type CreateAxiosDefaults } from 'axios'
+
+import {
+	getAccessToken,
+	removeFromStorage
+} from '@/services/auth-token.service'
+
+import { errorCatch } from './error'
+
+const options: CreateAxiosDefaults = {
+	baseURL: 'http://localhost:5000/api',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+	withCredentials: true
+}
+
+const axiosClassic = axios.create(options)
+
+const axiosWithAuth = axios.create(options)
+
+axiosWithAuth.interceptors.request.use(config => {
+	const accessToken = getAccessToken()
+
+	if (config?.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`
+
+	return config
+})
+
+axiosWithAuth.interceptors.response.use(
+	config => config,
+	async error => {
+		const originalRequest = error.config
+
+		if (
+			(error?.response?.staus === 401 ||
+				errorCatch(error) === 'jwt expired' ||
+				errorCatch(error) === 'jwt must be provided') &&
+			error.config &&
+			!error.config._isRetry
+		) {
+			originalRequest._isRetry = true
+			try {
+				await authService.getNewToken()
+				return axiosWithAuth.request(originalRequest)
+			} catch (error) {
+				if (errorCatch(error) === 'jwt expired') removeFromStorage()
+			}
+		}
+
+		throw error
+	}
+)
+
+export { axiosClassic, axiosWithAuth }
